c56e0b9bbbaf259ee77f3089d33d0c7371091ab7
ch-reboot <ch-reboot@protonmail.com>
2021-01-25 21:25:13 +0000

    2

--- start.php
+++ start.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "1";
+require 'vendor/autoload.php';
+
+$api = new Binance\API( "binance.json" );
+
+echo "<pre>";
+
+
+$ticker = $api->prices();
+print_r($ticker);
+
+
+
+
+
+
+echo "</pre>";
