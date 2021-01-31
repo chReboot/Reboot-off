887e0e8d1701eea5900acb256ae9ea5c82599ad3
chReboot <77994167+chReboot@users.noreply.github.com>
2021-01-31 21:29:53 +0100

    Update Logik.php

--- Logik.php
+++ Logik.php
@@ -146,7 +146,7 @@ class Logik {
         
         // BUY
         $einsatz = 0.3 * $balances['BTC']['available'];
-        $quantity = $einsatz / $ticker['$this->selectedSymbol'];
+        $quantity = $einsatz / $ticker[$this->selectedSymbol];
 
         print_r("Menge". $quantity);
 
